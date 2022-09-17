77d18eb77e5c50de96af53564307b6452d0d9ed7
Jaya <akulajayateja4444@gmail.com>
2022-09-17 18:04:06 +0530

    progress bar thing has been figured out

--- progressBar.js
+++ progressBar.js
@@ -1,3 +1,19 @@
 function whenClicked(){
     let input = document.getElementById('user-input').value;
-}   
+    let input1 = document.getElementById('user-input');
+    let bar = document.getElementById('bar');
+    var width = 0;
+
+    if(input <= 20){
+        bar.style.width = input + '%';
+        bar.style.background = "red";
+    } else if(input > 20 && input <= 60){
+        bar.style.width = input + '%';
+        bar.style.background = "yellow";
+    } else if(input > 60 && input <= 100){
+        bar.style.width = input + '%';
+        bar.style.background = "green";
+    } else{
+        alert("enter the number in between 1 and 100");
+    }
+}
