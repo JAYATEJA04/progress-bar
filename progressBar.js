73375f185def3b85bdd6403c0bf6187cc98b0693
Jaya <akulajayateja4444@gmail.com>
2022-09-19 21:23:51 +0530

    fixed the input if 100 or less than 0

--- progressBar.js
+++ progressBar.js
@@ -3,45 +3,17 @@ function whenClicked(){
     let input1 = document.getElementById('user-input');
     let bar = document.getElementById('progress');
     var width = 0;
-    // let id = setInterval(frame, 200);
-
-    // function frame(){
-
-    //     if(width >= 10){
-    //         clearInterval(id);
-    //     } else {
-    //         if(input <= 20){
-    //             bar.style.width = input + '%';
-    //             bar.style.background = "red";
-    //             width += input;
-    //         } else if(input > 20 && input <= 60){
-    //             bar.style.width = input + '%';
-    //             bar.style.background = "yellow";
-    //             width += input;
-    //         } else if(input > 60 && input <= 100){
-    //             bar.style.width = input + '%';
-    //             bar.style.background = "green";
-    //             width += input;
-    //         } else{
-    //             alert("enter the number in between 1 and 100");
-    //             width += input;
-    //         }
-    //     }
-    // }
     if(input <= 20){
-        bar.style.width = input + '%';
+        bar.style.width = 20 + '%';
         bar.style.background = "red";
         width += input;
     } else if(input > 20 && input <= 65){
-        bar.style.width = input + '%';
+        bar.style.width = 65 + '%';
         bar.style.background = "yellow";
         width += input;
-    } else if(input > 65 && input <= 100){
-        bar.style.width = input + '%';
+    } else if(input > 65){
+        bar.style.width = 99 + '%';
         bar.style.background = "green";
-        width += input;
-    } else{
-        alert("enter the number in between 1 and 100");
         width += input;
     }
 }
